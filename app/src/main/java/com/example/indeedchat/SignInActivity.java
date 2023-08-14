e2c2c42fd34fe3f6d3dca7bb7f232dbe10679952
Basudev <samantabasu.2001@gmail.com>
2023-08-14 20:07:12 +0530

    Modified in SignIn Activity

--- app/src/main/java/com/example/indeedchat/SignInActivity.java
+++ app/src/main/java/com/example/indeedchat/SignInActivity.java
@@ -10,5 +10,8 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //Hide toolbar
+        getSupportActionBar().hide();
     }
 }
